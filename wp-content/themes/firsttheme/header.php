80e06542db8d7059e16175949346e0e944849338
hogivano <ogivano06@gmail.com>
2018-01-11 20:11:06 +0700

    "coba atom"

--- wp-content/themes/firsttheme/header.php
+++ wp-content/themes/firsttheme/header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Hendriyan Ogivano</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link type="text/css" src="css/style.css">
 </head>
 <body>
     <header>
