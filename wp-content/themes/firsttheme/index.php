b91cb1841bb2c9b83617919ce9497e14d4c5b519
hogivano <ogivano06@gmail.com>
2018-01-12 16:32:59 +0700

    tutor 4

--- wp-content/themes/firsttheme/index.php
+++ wp-content/themes/firsttheme/index.php
@@ -1,5 +1,18 @@
-<?php get_header("cobaHead.php"); ?>
+<?php get_header(); ?>
 <div>
+    <?php
+        if (have_posts()): ?>
+        <ul class="list-group" >
+        <?php while (have_posts()):
+                the_post(); ?>
+                    <li id=<?php the_id(); ?> class="list-group-item">
+                        <h2><?php the_title(); ?></h2>
+                        <small>Post pada : <?php the_time( "j F Y" ); ?> Waktu : <?php the_time("g:i a"); ?> in <?php the_category() ?></small>
+                        <p><?php the_content(); ?></p>
+                    </li>
+        <?php   endwhile; ?>
+        </ul>
+    <?php endif; ?>
     Ini adalah tampilan index theme
 </div>
 <?php get_footer(); ?>
