b4294f47670c20a3eb75456989ca4ac6e7534055
hogivano <ogivano06@gmail.com>
2018-01-19 16:24:22 +0700

    change baseController

--- wp-content/plugins/event-plugin-hendri/inc/Base/BaseController.php
+++ wp-content/plugins/event-plugin-hendri/inc/Base/BaseController.php
@@ -17,7 +17,7 @@ class BaseController {
         $this->plugin = plugin_basename(dirname(__FILE__, 3) . "/event-plugin-hendri.php");
     }
 }
-// define ("PLUGIN_PATH", plugin_dir_path(__FILE__ ));
+// define ("PLUGIN_PATH", plugin_dir_pah(__FILE__ ));
 // define ("PLUGIN_URL", plugin_dir_url(__FILE__ ));
 // define ("PLUGIN", plugin_basename(__FILE__ ));
 ?>
